5e0b1ef78a9c0e8e41c89d6bf5a5f045e0d21feb
sarathsathish07 <sarathsathish1998@gmail.com>
2024-08-25 00:17:06 +0530

    token

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -7,13 +7,16 @@ const generateToken = (res,userId)=>{
   const token = jwt.sign({userId}, process.env.JWT_SECRET,{
     expiresIn:"30d",
   })
+  console.log('GenerateToken123:', token);
 
   res.cookie('jwt', token, {
     httpOnly: true,  
-    secure: process.env.NODE_ENV !== 'development',  
-    sameSite: 'strict',  
-    maxAge: 30 * 24 * 60 * 60 * 1000,  
+    secure: process.env.NODE_ENV === 'production',  
+    sameSite: 'lax',  
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    path: '/'
   })
+  
 }
 
 const sendOtpEmail = async (email, otp) => {
