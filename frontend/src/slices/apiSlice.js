904c79f2e1478d980b9410e29bfaf34424d5cd68
sarathsathish07 <sarathsathish1998@gmail.com>
2024-08-25 00:27:20 +0530

    cookie

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,9 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({baseUrl: 'https://celebratespaces.site'})
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://celebratespaces.site',
+  credentials: 'include', 
+});
 
 export const apiSlice = createApi({
   baseQuery,
