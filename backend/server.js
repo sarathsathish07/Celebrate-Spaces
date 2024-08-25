7220aec06c06c8273d1706ec1b1bfbcaec283eb5
sarathsathish07 <sarathsathish1998@gmail.com>
2024-08-25 17:33:42 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,12 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000","https://task-manager-app-7e1v-1qh2cbcx1-sarath-sathishs-projects.vercel.app"], 
-  credentials: true,  
+  origin: ["http://localhost:3000", "https://task-manager-app-7e1v-1qh2cbcx1-sarath-sathishs-projects.vercel.app"],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
